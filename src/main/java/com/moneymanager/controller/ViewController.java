3661e47158a0ddd897b184cb4a71e19dfcb7ff32
manohar <manohar@gmail.com>
2026-05-07 02:34:37 +0530

    final frontend serving fix

--- src/main/java/com/moneymanager/controller/ViewController.java
+++ src/main/java/com/moneymanager/controller/ViewController.java
@@ -1,13 +1,13 @@
 package com.moneymanager.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
 
-    @RequestMapping(value = {"/", "/login", "/dashboard"})
+    @GetMapping("/")
     public String index() {
-        return "forward:/index.html";
+        return "forward:/build/index.html";
     }
 }
