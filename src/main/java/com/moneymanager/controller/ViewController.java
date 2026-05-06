4c6d58fcbd7e920f02276a96bed205d70cf3862e
manohar <manohar@gmail.com>
2026-05-07 03:10:39 +0530

    fixed railway routing

--- src/main/java/com/moneymanager/controller/ViewController.java
+++ src/main/java/com/moneymanager/controller/ViewController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ViewController {
 
-    @GetMapping("/")
+    @GetMapping(value = "/")
     public String index() {
-        return "forward:/build/index.html";
+        return "forward:/index.html";
     }
 }
