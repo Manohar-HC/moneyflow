f43542a8be47c9567e4ba75a27fd08a9e4753f18
manohar <manohar@gmail.com>
2026-05-06 21:14:06 +0530

    final live fullstack app

---
+++ src/main/java/com/moneymanager/controller/ViewController.java
@@ -0,0 +1,13 @@
+package com.moneymanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "forward:/build/index.html";
+    }
+}
